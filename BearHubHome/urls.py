1196ffe9f23a0f3e1641d65d6b1a3b8f15dcb6b0
kevinandrawes <kevin212007@icloud.com>
2023-02-12 16:56:13 -0600

    Way to obtain user pass and id

--- BearHubHome/urls.py
+++ BearHubHome/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name='bear'
 urlpatterns = [
 path("" ,views.index, name = "index"),
 path("kevin" , views.kevin,  name = "kevin"),
--- BearHubHome/views.py
+++ BearHubHome/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
-
+from django import forms
 # Create your views here.
+class LogInForm(forms.Form):
+    id = forms.CharField(label="id")
+    password = forms.CharField(label="password")
 def index(request):
-    return render(request,"HII/index.html")
+    if request.method=="POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data.get("password")
+            id = form.cleaned_data.get("id")
+
+        else:
+            form = LogInForm()
+    return render(request,"HII/index.html",{"form":LogInForm()})
 
 def kevin(request):
     return HttpResponse("Hello Kevin")
@@ -15,3 +26,5 @@ def greet(request, name ):
     }
     return render(request,"HII/greet.html",data)
 
+
+  
