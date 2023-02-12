6982ab380aa67f53026a892a8b3da5fbf3ca59a3
kevinandrawes <kevin212007@icloud.com>
2023-02-12 14:44:08 -0600

    git

--- BearHubHome/models.py
+++ BearHubHome/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class Student(models.Model):
+    First_name = models.CharField(max_length=64)
+    Last_name = models.CharField(max_length=64)
+    grade_level = models.IntegerField()
+    password= models.CharField(max_length=64)
 # Create your models here.
