eeae35fe0fde551b23b17aa97293b1de7f541b86
kevinandrawes <kevin212007@icloud.com>
2023-03-14 14:10:07 -0500

    Removed Kevin Path

--- BearHubHome/urls.py
+++ BearHubHome/urls.py
@@ -6,7 +6,6 @@ app_name='bear'
 urlpatterns = [
 path("",views.index,name="index"),
 path("LogIn" ,views.LogIn, name = "LogIn"),
-path("kevin" , views.kevin,  name = "kevin"),
 path('StudentPage/<int:user_id>/',views.StudentPage,name="stupage"),
 path('SignUp',views.SignUp,name="signUp"),
 path('AdminLogIn',views.adminLogIn,name="adminLogIn"),
