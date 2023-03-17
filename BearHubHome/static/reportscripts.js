e20ce1064b449ed8d5632e583ca1b696b3f6a04d
alentheguy <alvinw0231@gmail.com>
2023-03-16 22:34:07 -0500

    progress of changing everyhign for the better!!!

--- BearHubHome/static/reportscripts.js
+++ BearHubHome/static/reportscripts.js
@@ -1,52 +1,20 @@
-// Get the data from the Django view and pass it to the JavaScript file
-var users = "{{ users|safe }}";
-var users9 = "{{ users9|safe }}";
-var users10 = "{{ users10|safe }}";
-var users11 = "{{ users11|safe }}";
-var users12 = "{{ users12|safe }}";
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-// Create an array of labels for the chart
-var labels = ["9th Grade", "10th Grade", "11th Grade", "12th Grade"];
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-// Create an array of data for the chart
-var data = [users9.length, users10.length, users11.length, users12.length];
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-// Create a function to generate the chart
-function generateChart() {
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Number of Points per Grade',
-                data: data,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
-
-// Call the function to generate the chart
-generateChart();
--- BearHubHome/static/scripts.js
+++ BearHubHome/static/scripts.js
@@ -1,13 +1,24 @@
 function showInfo() {
     var infoWindow = document.getElementById("infoWindow");
     infoWindow.classList.add("show");
-    }
+}
 
 function hideInfo() {
     var infoWindow = document.getElementById("infoWindow");
     infoWindow.classList.remove("show");
-    }
+}
+var infoButton = document.querySelector(".topnav button");
+infoButton.addEventListener("mouseover", showInfo);
+infoButton.addEventListener("mouseout", hideInfo);
 
-    var infoButton = document.querySelector(".topnav button");
-    infoButton.addEventListener("mouseover", showInfo);
-    infoButton.addEventListener("mouseout", hideInfo);
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
